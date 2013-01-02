f08ef6dff61df3512d4b7dd3b59c27817ccd228f
yagi <sylc.yagi@gmail.com>
2013-01-03 05:11:10 +0900

    add test code.

--- android_model_provider_sample/src/jp/dip/sys1/yagi/amp/sample/dao/ItemDaoBase.java
+++ android_model_provider_sample/src/jp/dip/sys1/yagi/amp/sample/dao/ItemDaoBase.java
@@ -2,8 +2,28 @@ package jp.dip.sys1.yagi.amp.sample.dao;
 
 public class ItemDaoBase extends DAOBase {
 
+    private String name;
+    private String title;
+    
     public ItemDaoBase() {
 
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    
 }
---
+++ android_model_provider_sample/tests/jp/dip/sys1/yagi/amp/sample/dao/ItemDaoBaseTest.java
@@ -0,0 +1,13 @@
+package jp.dip.sys1.yagi.amp.sample.dao;
+
+
+import junit.framework.TestCase;
+
+public class ItemDaoBaseTest extends TestCase {
+
+    public void testGetName(){
+        
+        assertTrue(true);
+        //assertThat(10, is(10));
+    }
+}
