76a8bd01f3fdc6f5fceb1d013855295f4d2c9bca
yagi <sylc.yagi@gmail.com>
2013-01-03 17:38:28 +0900

    change tesing framework.

---
+++ android_model_provider_sample/build_and_run.sh
@@ -0,0 +1,3 @@
+ant debug
+ant installd
+ant test -Dtest.runner=com.android.test.runner.AndroidJUnitRunner
--- android_model_provider_sample/tests/jp/dip/sys1/yagi/amp/sample/dao/ItemDaoBaseTest.java
+++ android_model_provider_sample/tests/jp/dip/sys1/yagi/amp/sample/dao/ItemDaoBaseTest.java
@@ -1,22 +1,13 @@
 package jp.dip.sys1.yagi.amp.sample.dao;
 
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
-import com.uphyca.testing.AndroidJUnit4TestAdapter;
-import com.uphyca.testing.AndroidTestCase;
+public class ItemDaoBaseTest{
 
-public class ItemDaoBaseTest extends AndroidTestCase{
-
-    /**
-     * For Eclipse with ADT
-     */
-    public static junit.framework.Test suite() {
-        return new AndroidJUnit4TestAdapter(ItemDaoBaseTest.class);
-    }
     @Test
     public void testGetName(){
         assertThat(10, is(10));
