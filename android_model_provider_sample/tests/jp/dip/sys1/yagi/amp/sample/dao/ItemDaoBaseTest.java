94ff9fbf77e4a644a9dd84efc53dfcef01d323d0
yagi <sylc.yagi@gmail.com>
2013-01-03 15:00:07 +0900

    swap junit.jar! and add Testcase.

--- android_model_provider_sample/tests/jp/dip/sys1/yagi/amp/sample/dao/ItemDaoBaseTest.java
+++ android_model_provider_sample/tests/jp/dip/sys1/yagi/amp/sample/dao/ItemDaoBaseTest.java
@@ -2,6 +2,7 @@ package jp.dip.sys1.yagi.amp.sample.dao;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
@@ -19,5 +20,13 @@ public class ItemDaoBaseTest extends AndroidTestCase{
     @Test
     public void testGetName(){
         assertThat(10, is(10));
+    }
+    @Test
+    public void 足し算5_plus_5(){
+        assertThat(5+5, is(10));
+    }
+    @Test
+    public void 足し算5_plus_10(){
+        assertThat(5+10, is(15));
     }
 }
